e19369e020718c7e802c649e024537ba873cb788
Scruffy pc <cayabyabrr@yahoo.com>
2024-03-08 23:33:50 -0800

    works with vpn

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const https = require('https');
 
 // URL of the webpage you want to scrape
 const url = "https://beta.boomerang.trade/opp";
 // const url = "https://rainbowdarkness.com";
 
+const agent = new https.Agent({
+    rejectUnauthorized: false,
+  });
+
 // Function to fetch and scrape the webpage
 async function scrapeWebpage() {
   try {
     // Make a GET request to the webpage
-    const response = await axios.get(url);
+    const response = await axios.get(url, { httpsAgent: agent });
 
     // Load the HTML content into Cheerio
     const $ = cheerio.load(response.data); 
 
-    // Example: Extracting values from elements with a class of 'percentage'
-    const percentageValues = [];
-    $('.percentage').each((index, element) => {
-      // Extract the text content and parse the number (remove % sign)
-      const percentage = parseFloat($(element).text().replace('%', ''));
-      percentageValues.push(percentage);
-    });
 
-    // Log the extracted values
-    console.log('Percentage Values:', percentageValues);
+    // $(
+    //     "#div1"
+    // ).each((i, e) => {
+    //     console.log(e + "test")
+    // })
+
+    console.log($("#root > div > div.bg-\\[\\#0e1424\\].font-bricolagegrotesques > nav > div.w-\\[70\\%\\].lg\\:text-\\[12\\.5px\\].\\32 xl\\:text-\\[15\\.5px\\].flex.items-center"))
+    // console.log($("#div1 > div > div:nth-child(1) > div"))
+
+    // "#div1 > div > div:nth-child(1) > div > div.border.border-gray-700.py-1.rounded-b-3xl.flex.items-center.justify-center"
+    // document.querySelector("#div1 > div > div:nth-child(1) > div > div.border.border-gray-700.py-1.rounded-b-3xl.flex.items-center.justify-center > p"
   } catch (error) {
     console.error('Error:', error.message);
   }
